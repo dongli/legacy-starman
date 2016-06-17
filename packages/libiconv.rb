4e4331c5f8e1d611fe72278172c162be3ca6d4c1
Li Dong <dongli@lasg.iap.ac.cn>
2016-06-17 10:49:20 +0800

    Fix system conflict of libiconv in Mac

--- packages/libiconv.rb
+++ packages/libiconv.rb
@@ -5,6 +5,9 @@ module STARMAN
     sha256 '72b24ded17d687193c3366d0ebe7cde1e6b18f0df8c55438ac95be39e8a30613'
     version '1.14'
 
+    # Mac ships one with no 'lib' prefix in some symbols.
+    label :system_conflict if OS.mac?
+
     def install
       args = %W[
         --prefix=#{prefix}
