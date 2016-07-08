81837ff121156f81304e859092246cf0ae05acb2
Li Dong <dongli@lasg.iap.ac.cn>
2016-07-08 20:09:50 +0800

    Add icu4c 57.1

---
+++ packages/icu4c.rb
@@ -0,0 +1,23 @@
+module STARMAN
+  class Icu4c < Package
+    homepage 'http://site.icu-project.org/'
+    url 'https://ssl.icu-project.org/files/icu4c/57.1/icu4c-57_1-src.tgz'
+    sha256 'ff8c67cb65949b1e7808f2359f2b80f722697048e90e7cfc382ec1fe229e9581'
+    version '57.1'
+
+    def install
+      args = %W[
+        --prefix=#{prefix}
+        --disable-samples
+        --disable-tests
+        --enable-static
+        --with-library-bits=64
+      ]
+      work_in 'source' do
+        run './configure', *args
+        run 'make'
+        run 'make', 'install'
+      end
+    end
+  end
+end
