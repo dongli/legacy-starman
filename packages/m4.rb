7bdbdb023852c8f253d417bda77ece32b50dda11
Li Dong <dongli@lasg.iap.ac.cn>
2016-09-28 17:51:44 +0800

    Forget to add version to m4

--- packages/m4.rb
+++ packages/m4.rb
@@ -3,6 +3,7 @@ module STARMAN
     homepage 'https://www.gnu.org/software/m4'
     url 'http://ftp.gnu.org/gnu/m4/m4-1.4.17.tar.gz'
     sha256 '3ce725133ee552b8b4baca7837fb772940b25e81b2a9dc92537aeaf733538c9e'
+    version '1.4.17'
 
     label :compiler_agnostic
 
