2269e9f95a5ed01dd63ff95154e4d5d150c4d260
Li Dong <dongli@lasg.iap.ac.cn>
2016-11-28 10:31:09 +0800

    Fix version gsub in centos

--- framework/system/os/linux.rb
+++ framework/system/os/linux.rb
@@ -1,7 +1,7 @@
 module STARMAN
   class Linux < OS
     version do
-      `lsb_release -r`.gsub(/^.*\s+/, '').chomp
+      `lsb_release -r`.gsub(/^\D*/, '').chomp
     end
     soname :so
     ld_library_path 'LD_LIBRARY_PATH'
--- packages/icu4c.rb
+++ packages/icu4c.rb
@@ -6,16 +6,19 @@ module STARMAN
     version '57.1'
 
     def install
+      # Add --enable-rpath to allow icu4c library paths to be embeded into other files.
       args = %W[
         --prefix=#{prefix}
         --disable-samples
         --disable-tests
         --enable-static
+        --enable-rpath
         --with-library-bits=64
       ]
       work_in 'source' do
         run './configure', *args
         run 'make'
+        run 'make', 'test' unless skip_test?
         run 'make', 'install'
       end
     end
