ee3167e316f9827534d4b65f9076b00d12166612
Li Dong <dongli@lasg.iap.ac.cn>
2016-12-21 22:58:52 +0800

    Libxml2 depends on zlib

--- packages/libxml2.rb
+++ packages/libxml2.rb
@@ -5,12 +5,15 @@ module STARMAN
     sha256 'ffb911191e509b966deb55de705387f14156e1a56b21824357cdf0053233633c'
     version '2.9.4'
 
+    depends_on :zlib
+
     def install
       args = %W[
         --prefix=#{prefix}
         --disable-dependency-tracking
         --without-python
         --without-lzma
+        --with-zlib=#{Zlib.prefix}
       ]
       run './configure', *args
       run 'make'
