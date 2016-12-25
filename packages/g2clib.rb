585161689ca31f8e2ed1c307daee4a8f6cbe2523
Li Dong <dongli@lasg.iap.ac.cn>
2016-12-25 18:24:03 +0800

    Add g2clib 1.6.0

---
+++ packages/g2clib.rb
@@ -0,0 +1,23 @@
+module STARMAN
+  class G2clib < Package
+    url 'http://www.nco.ncep.noaa.gov/pmb/codes/GRIB2/g2clib-1.6.0.tar'
+    sha256 'afec1ea29979b84369d0f46f305ed12f73f1450ec2db737664ec7f75c1163add'
+    version '1.6.0'
+
+    depends_on :jasper
+    depends_on :libpng
+    depends_on :zlib
+
+    def install
+      inreplace 'makefile', {
+        'INC=-I/nwprod/lib/include/' => "INC=-I#{Jasper.inc} -I#{Libpng.inc} -I#{Zlib.inc}",
+        'CC=gcc' => "CC=#{CompilerStore.compiler(:c).command}"
+      }
+      run 'make'
+      mkdir_p inc
+      cp 'grib2.h', inc
+      mkdir_p lib
+      cp 'libg2c_v1.6.0.a', lib
+    end
+  end
+end
