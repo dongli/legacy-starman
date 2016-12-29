a219270c6a4ab971fe89cf5ee68b930752a612b1
Li Dong <dongli@lasg.iap.ac.cn>
2016-12-29 13:35:26 +0800

    Fix a bug

--- framework/system/os/mac.rb
+++ framework/system/os/mac.rb
@@ -2,7 +2,7 @@ module STARMAN
   class Mac < OS
     type :mac
     version do
-      `sw_vers`.match(/ProductVersion:\s*(\d+\.\d+(\.\d+)?)/)[1]
+      `sw_vers`.match(/ProductVersion:\s*(\d+\.\d+(\.\d+)?)/)[1] rescue nil
     end
     soname :dylib
     ld_library_path 'DYLD_LIBRARY_PATH'
