47565f5f087fe14401e166e68cb23ff6f4ee7f19
Li Dong <dongli@lasg.iap.ac.cn>
2017-01-02 13:58:55 +0800

    Add Rakefile and config relative_assets

---
+++ Rakefile
@@ -0,0 +1,3 @@
+require 'middleman-gh-pages'
+
+ENV['ALLOW_DIRTY'] = 'true'
--- config.rb
+++ config.rb
@@ -17,6 +17,8 @@ page '/*.txt', layout: false
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
 # General configuration
+activate :relative_assets
+set :relative_links, true
 
 # Reload the browser automatically whenever files change
 configure :development do
@@ -37,8 +39,8 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 end
