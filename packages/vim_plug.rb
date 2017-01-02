6253340dec1b9dffbc553c57327275081ab15a00
Li Dong <dongli@lasg.iap.ac.cn>
2017-01-02 10:22:21 +0800

    Update vim_plug

--- packages/vim_plug.rb
+++ packages/vim_plug.rb
@@ -9,25 +9,44 @@ module STARMAN
 
     depends_on :vim
 
-    resource :neocomplete do
-
-    end
-
     def install
       mkdir_p "#{prefix}/autoload"
       mkdir_p "#{persist}/plugged"
       cp 'plug.vim', "#{prefix}/autoload"
-      append_file "#{ENV['HOME']}/.vimrc", <<-EOT.keep_indent
-        " Added by STARMAN
-        set runtimepath+=#{prefix}
+    end
 
-        call plug#begin('#{persist}/plugged')
+    def post_install
+      vimrc = "#{ENV['HOME']}/.vimrc"
+      if not File.exist? vimrc or File.read(vimrc).include?(/" +++ Plug.vim settings added by STARMAN +++/)
+        append_file "#{ENV['HOME']}/.vimrc", <<-EOT.keep_indent
+          " +++ Plug.vim settings added by STARMAN +++
+          set runtimepath+=#{prefix}
 
-        Plug 'Shougo/neocomplete.vim'
-        " Add your favorite plugins here, and run :PlugInstall
+          call plug#begin('#{persist}/plugged')
 
-        call plug#end()
-      EOT
+          Plug 'Shougo/neocomplete.vim'
+          Plug 'Shougo/neosnippet.vim'
+          Plug 'Shougo/neosnippet-snippets'
+          " Add your favorite plugins here, and run :PlugInstall
+
+          call plug#end()
+
+          " +++ Neocomplete.vim settings +++
+          let g:neocomplete#enable_at_startup = 1
+          let g:neocomplete#enable_smart_case = 1
+          inoremap <expr><Left>  neocomplete#close_popup() . "\<Left>"
+          inoremap <expr><Right> neocomplete#close_popup() . "\<Right>"
+          inoremap <expr><Up>    neocomplete#close_popup() . "\<Up>"
+          inoremap <expr><Down>  neocomplete#close_popup() . "\<Down>"
+          imap <expr><TAB> neosnippet#expandable_or_jumpable() ?
+            \ "\<Plug>(neosnippet_expand_or_jump)"
+            \: pumvisible() ? "\<C-n>" : "\<TAB>"
+          smap <expr><TAB> neosnippet#expandable_or_jumpable() ?
+            \ "\<Plug>(neosnippet_expand_or_jump)"
+            \: "\<TAB>"
+        EOT
+        CLI.report_notice "Run #{CLI.blue ':PlugInstall'} in VIM."
+      end
     end
   end
 end
