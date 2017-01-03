64b6584d8e4e7453e0ab72746f7e437edcc70346
Li Dong <dongli@lasg.iap.ac.cn>
2017-01-03 10:03:41 +0800

    Fix berkeleydb4 compiled by clang

--- packages/berkeleydb4.rb
+++ packages/berkeleydb4.rb
@@ -7,6 +7,8 @@ module STARMAN
 
     label :compiler_agnostic
 
+    patch :DATA if CompilerStore.compiler(:c).vendor == :llvm
+
     def install
       args = %W[
         --prefix=#{prefix}
@@ -20,3 +22,26 @@ module STARMAN
     end
   end
 end
+
+__END__
+diff -Naur a/dbinc/atomic.h b/dbinc/atomic.h
+--- a/dbinc/atomic.h	2010-04-13 04:25:22.000000000 +0800
++++ b/dbinc/atomic.h	2017-01-03 09:58:09.000000000 +0800
+@@ -144,7 +144,7 @@
+ #define	atomic_inc(env, p)	__atomic_inc(p)
+ #define	atomic_dec(env, p)	__atomic_dec(p)
+ #define	atomic_compare_exchange(env, p, o, n)	\
+-	__atomic_compare_exchange((p), (o), (n))
++	__atomic_compare_exchange_db((p), (o), (n))
+ static inline int __atomic_inc(db_atomic_t *p)
+ {
+ 	int	temp;
+@@ -176,7 +176,7 @@
+  * http://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html
+  * which configure could be changed to use.
+  */
+-static inline int __atomic_compare_exchange(
++static inline int __atomic_compare_exchange_db(
+ 	db_atomic_t *p, atomic_value_t oldval, atomic_value_t newval)
+ {
+ 	atomic_value_t was;
