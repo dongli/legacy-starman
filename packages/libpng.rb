4375c4d2e5f626f933af6805dd777e280a040615
Li Dong <dongli@lasg.iap.ac.cn>
2017-01-12 11:27:21 +0800

    Fix libpng

--- packages/libpng.rb
+++ packages/libpng.rb
@@ -1,9 +1,10 @@
 module STARMAN
   class Libpng < Package
     homepage 'http://www.libpng.org/pub/png/libpng.html'
-    url 'https://sourceforge.net/projects/libpng/files/libpng16/1.6.26/libpng-1.6.26.tar.xz'
+    url 'https://bintray.com/starman/backup/download_file?file_path=libpng-1.6.26.tar.xz'
     sha256 '266743a326986c3dbcee9d89b640595f6b16a293fd02b37d8c91348d317b73f9'
     version '1.6.26'
+    filename 'libpng-1.6.26.tar.xz'
 
     label :compiler_agnostic
     label :system_conflict
