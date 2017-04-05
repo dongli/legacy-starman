ac151d2c8a85845ba2d6b5c330e8ce0b5ee6c3f8
Li Dong <dongli@lasg.iap.ac.cn>
2017-04-05 13:21:48 +0800

    Update ncl to 6.4.0

--- packages/ncl.rb
+++ packages/ncl.rb
@@ -1,6 +1,6 @@
 module STARMAN
   class Ncl < Package
-    version '6.3.0'
+    version '6.4.0'
 
     label :external_binary
     label :compiler_agnostic
@@ -11,15 +11,27 @@ module STARMAN
     end
 
     external_binary_on :mac, '>= 10.11' do
-      url 'https://www.earthsystemgrid.org/download/fileDownload.html?logicalFileId=fa161bc1-84c7-11e6-8aa0-00c0f03d5b7c'
-      sha256 '69a11cef0a3d0af78a07ad7acf4769bc8f22ad3ac22d391d5223047824d1daff'
-      filename 'ncl_ncarg-6.3.0.MacOS_10.11_64bit_gcc610.tar.gz'
+      url 'https://www.earthsystemgrid.org/download/fileDownload.html?logicalFileId=1139ad88-fa02-11e6-a976-00c0f03d5b7c'
+      sha256 '2e1a2957dacd14835716f0f7309117a35e1f6255fa8569d0dc3038c42df9cbfd'
+      filename 'ncl_ncarg-6.4.0-MacOS_10.11_64bit_gnu610.tar.gz'
+    end
+
+    external_binary_on :centos, '=~ 6.0' do
+      url 'https://www.earthsystemgrid.org/download/fileDownload.html?logicalFileId=fcdc5c23-fa01-11e6-a976-00c0f03d5b7c'
+      sha256 '82fd49d2a49458c783b50fcda96945949132ccab372b2694427753475059400f'
+      filename 'ncl_ncarg-6.4.0-CentOS6.8_64bit_gnu447.tar.gz'
     end
 
     external_binary_on :centos, '>= 7.0' do
-      url 'https://www.earthsystemgrid.org/download/fileDownload.html?logicalFileId=e083a923-cd9a-11e4-bb80-00c0f03d5b7c'
-      sha256 'd4ee2904d95ce1e86b5c7bafcf14b9222642c5e4d60d10972b3bb57b10ede131'
-      filename 'ncl_ncarg-6.3.0.Linux_CentOS7.0_x86_64_gcc482.tar.gz'
+      url 'https://www.earthsystemgrid.org/download/fileDownload.html?logicalFileId=191cb88a-fa02-11e6-a976-00c0f03d5b7c'
+      sha256 '85620a87e626bef385d0b0c821a3de147c41fc4b8170be64d5757eb60d0695dc'
+      filename 'ncl_ncarg-6.4.0-CentOS7.3_64bit_gnu485.tar.gz'
+    end
+
+    external_binary_on :ubuntu, '>= 12' do
+      url 'https://www.earthsystemgrid.org/download/fileDownload.html?logicalFileId=154af009-fa02-11e6-a976-00c0f03d5b7c'
+      sha256 'ebd7365243bf2b36ef67937333d969138ad59b17f9e637ace2b747538c5ef256'
+      filename 'ncl_ncarg-6.4.0-Debian8.6_64bit_gnu492.tar.gz'
     end
 
     def export_env
