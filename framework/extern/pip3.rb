879aa164f373893d2e6a56c2720c4fc1ee69b001
Li Dong <dongli@lasg.iap.ac.cn>
2017-04-11 15:27:57 +0800

    Add pip3 install command

--- framework/extern/pip3.rb
+++ framework/extern/pip3.rb
@@ -8,7 +8,7 @@ module STARMAN
       args = CommandLine.option(:command).split
       CLI.report_error "Command is not #{CLI.red 'pip3'}!" if args.shift != 'pip3'
       @@command = args.shift.to_sym
-      CLI.report_error "Unknown pip3 command #{CLI.red @@command}}" unless [:download].include? @@command
+      CLI.report_error "Unknown pip3 command #{CLI.red @@command}}" unless [:download, :install].include? @@command
       @@options = []
       @@wheels = []
       args.each do |arg|
@@ -31,13 +31,20 @@ module STARMAN
           run 'pip3', 'download', wheel
           RemoteServer.instances.each do |name, server|
             package_root = ConfigStore.config[:remote][name][:starman][:package_root]
-            Dir.glob('*.whl') do |filename|
+            Dir.glob('*') do |filename|
               remote_file = "#{package_root}/#{filename}"
               next if sha_same? filename, server.sha256(remote_file)
               server.upload filename, remote_file, file: true
             end
           end
         end
+      end
+    end
+
+    def self.install
+      # Assume we are in offline server.
+      @@wheels.each do |wheel|
+        run 'pip3', 'install', '--no-index', "--find-links=#{ConfigStore.package_root}", wheel
       end
     end
   end
