9f05604e1b3b456b746fbe8e7da656ca94624c33
Li Dong <dongli@lasg.iap.ac.cn>
2017-05-06 10:30:47 +0800

    Check editor when config

--- framework/command/config.rb
+++ framework/command/config.rb
@@ -1,13 +1,20 @@
 module STARMAN
   module Command
     class Config
+      extend System::Command
+
       def self.accepted_options
         {}
       end
 
       def self.__run__
-        editor = 'vim'
-        system "#{editor} -c 'set filetype=yaml' #{CommandLine.options[:config].value}"
+        if system_command? 'vim'
+          system "vim -c 'set filetype=yaml' #{CommandLine.option(:config)}"
+        elsif system_command? 'vi'
+          system "vi #{CommandLine.option(:config)}"
+        else
+          CLI.report_error 'Do not know which editor to use.'
+        end
       end
     end
   end
