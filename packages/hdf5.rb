3dcc6abaa0a93959bf3bee1e7914d9380be2353d
Li Dong <dongli@lasg.iap.ac.cn>
2017-12-27 17:03:53 +0800

    update hdf5 url

--- packages/hdf5.rb
+++ packages/hdf5.rb
@@ -1,7 +1,7 @@
 module STARMAN
   class Hdf5 < Package
     homepage 'http://www.hdfgroup.org/HDF5'
-    url 'https://support.hdfgroup.org/ftp/HDF5/current18/src/hdf5-1.8.19.tar.bz2'
+    url 'https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.19/src/hdf5-1.8.19.tar.bz2'
     sha256 '59c03816105d57990329537ad1049ba22c2b8afe1890085f0c022b75f1727238'
     version '1.8.19'
 
